e69a614bf08a18cfa3600f350430c295cfd1b060
EasonKuo <eason.kuo@addweup.com>
2018-11-19 14:52:43 +0800

    加入所有國籍的旗幟

--- parse_flag_from_freeflag.py
+++ parse_flag_from_freeflag.py
@@ -2,9 +2,9 @@
 
 import os
 import csv
-from
 import requests
-from lxml import etree
+from PIL import Image
+from StringIO import StringIO
 from bs4 import BeautifulSoup
 import pandas as pd
 
@@ -23,30 +23,36 @@ def save_flag_to_file(flag_url, filename):
     folder_path = os.path.join(basepath, save_folder)
     abs_file_path = os.path.abspath(os.path.join(folder_path, filename))
 
-    with open(abs_file_path, 'wb') as f:
-        if 'http:' not in flag_url:
-            flag_url = '{}{}'.format('http:', flag_url)
-            
-        resp = requests.get(flag_url)
-        f.write(resp.content)
+    # 調整大小與保存
+    resp = requests.get(flag_url)
+    im = Image.open(StringIO(resp.content))
+    im_width, im_height = im.size
+    width = 255
+    ratio = im_width / width
+    height = im_height / ratio
+    im = im.resize((width, height), Image.BILINEAR)
+    im.save(abs_file_path)
 
 
 def grab_country_data(code_row):
     data = code_row.find_all('td')
     INTRO_LINK_IDX = 0
+    COUNTRY_NAME = 1
     COUNTRY_LTD_IDX = 5
-    import pdb; pdb.set_trace()
     flag_url = site + data[INTRO_LINK_IDX].find('a').get('href')
     tld = data[COUNTRY_LTD_IDX].text
-    abbr2_iso = tld[1:].upper()
-    print("導向的網址: {}, LTD 碼: {}, Abbr2 iSO: {} ".format(flag_url, tld, abbr2_iso))
-    flag_path = save_flag_to_file(flag_url, abbr2_iso + '.png')
+    if len(tld) != 3:
+        name = data[COUNTRY_NAME].text
+        print("導向的網址: {}, LTD 碼, 名稱: {} ".format(flag_url, name))
+        flag_path = save_flag_to_file(flag_url, name + '.png')
+    else:
+        abbr2_iso = tld[1:].upper()
+        print("導向的網址: {}, LTD 碼: {}, Abbr2 iSO: {} ".format(flag_url, tld, abbr2_iso))
+        flag_path = save_flag_to_file(flag_url, abbr2_iso + '.png')
 
     return {
         'flag_url': flag_url,
         'file_path': flag_path,
-        'tld': tld,
-        'abbr2_iso': abbr2_iso
     }
 
 
@@ -62,7 +68,7 @@ def main():
     rows_size = len(code_rows)
     for code_row in code_rows[DATA_START_ROW:rows_size - DATA_START_ROW]:
         grab_country_data(code_row)
-    print('已完成')
+    print('已完成!......')
 
 
 if __name__ == '__main__':
--- parse_flag_from_wiki.py
+++ parse_flag_from_wiki.py
@@ -70,49 +70,6 @@ def main():
         grab_country_data(code_row)
     print('已完成')
 
-    # countries_block = table.find("tbody").find_all("tr")
-    # country_list = []
-    # country_field = []
-    # header_columns = table_rows[COLUMN_FIELD_IDX].find_all('td')
-    # import pdb; pdb.set_trace()
-    # country_field.append("Flag")
-    # col_size = len(header_columns)
-    # for col_header in header_columns[COLUMN_FIELD_IDX + 1, col_size - 1]:
-    #     country_field.append(col_header.text)
-    #     key_size = len(country_field)
-
-    # for row in table_rows[1:]:
-    #     table_cols = row.find_all('td')
-    #     size = len(table_cols)
-
-    #     country_list.append({
-    #         'Country': (table_cols[size-4].text).strip(),
-    #         country_field[1]: (table_cols[size-3].text).strip(),
-    #         country_field[2]: (table_cols[size-2].text).strip(),
-    #     })
-
-    # basepath = os.path.dirname(__file__)
-    # reader_filepath = os.path.abspath(os.path.join(basepath, 'countries_data.csv'))
-    # writer_filepath = os.path.abspath(os.path.join(basepath, 'countries_code_out.csv'))
-
-    # # output = open('test2.csv','wb')
-    # # csvfile = open(reader_filepath)
-
-    # read_df = pd.read_csv(reader_filepath)
-    # print read_df
-    # import pdb; pdb.set_trace()
-    # country_code_df = pd.DataFrame(country_list)
-    # print country_code_df
-    # result_df = pd.merge(read_df, country_code_df, on='Country',how='left')
-    # print result_df
-    # result_df.to_csv(writer_filepath, sep=',', encoding='utf-8')
-    # # reader = csv.DictReader(csvfile, delimiter=',', quotechar='"')
-    # # ori_fieldnames = reader.fieldnames
-    # # ori_fieldnames.extend(country_field[1:])
-    # # import pdb; pdb.set_trace()
-    # # csvwriter = csv.DictWriter(output, delimiter=',', fieldnames=ori_fieldnames)
-
-    # # for row in UnicodeReader(fcsv, delimiter=',', quotechar='"'): # 2
 
 if __name__ == '__main__':
     main()
